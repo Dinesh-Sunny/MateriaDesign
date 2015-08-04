c9343f7961e6f18fcb1210c752f9f7b99cc55616
NazishSanaKhan <nazusana9594@gmail.com>
2015-08-05 01:18:54 +0530

    Navigation Drawer completed, Recycler view also done in drawer displaying elements

--- app/src/main/java/in/teachcoder/app/musicplayer/NavigationDrawerFragment.java
+++ app/src/main/java/in/teachcoder/app/musicplayer/NavigationDrawerFragment.java
@@ -5,10 +5,15 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Macbook on 04/08/15.
@@ -16,10 +21,34 @@ import android.view.ViewGroup;
 public class NavigationDrawerFragment extends Fragment {
     private  ActionBarDrawerToggle mDrawerToggle;
     private DrawerLayout mDrawerLayout;
+    private RecyclerView recyclerView;
+    private NavigationDrawerRecycleAdapter adapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.navigation_drawer_frag_layout, container, false);
+        //getting RecyclerView from Navigation Drawer
+        View layout = inflater.inflate(R.layout.navigation_drawer_frag_layout, container, false);
+        recyclerView = (RecyclerView) layout.findViewById(R.id.drawerList);
+
+        adapter = new NavigationDrawerRecycleAdapter(getActivity(),getData());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        return layout;
+    }
+
+    public static List<MusicFile> getData(){
+
+        List<MusicFile> data = new ArrayList<>();
+        String[] tvArray = {"baahubali","patanahi","aajao beta","hum se na hopayega"};
+
+        for(int i=0;i<tvArray.length;i++){
+
+            MusicFile musocFileObject = new MusicFile();
+            musocFileObject.fileName = tvArray[i];
+            data.add(musocFileObject);
+        }
+        return data;
     }
 
     public void setUp(DrawerLayout drawerLayout, final Toolbar toolBar) {
